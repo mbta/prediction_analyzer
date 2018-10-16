27e2f2b7aefb061993fa5b0a66a2b803e3ae8642
Alex Sghia-Hughes <alex@mbtace.com>
2018-10-16 13:54:18 -0400

    initial commit

---
+++ lib/prediction_analyzer.ex
@@ -0,0 +1,18 @@
+defmodule PredictionAnalyzer do
+  @moduledoc """
+  Documentation for PredictionAnalyzer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PredictionAnalyzer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prediction_analyzer_test.exs
@@ -0,0 +1,8 @@
+defmodule PredictionAnalyzerTest do
+  use ExUnit.Case
+  doctest PredictionAnalyzer
+
+  test "greets the world" do
+    assert PredictionAnalyzer.hello() == :world
+  end
+end
