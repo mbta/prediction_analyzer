cb051b96273ed68a14c42a4202cc15a5a66c9da2
Alex Sghia-Hughes <alex@mbtace.com>
2018-10-17 16:16:39 -0400

    migration

---
+++ priv/repo/migrations/20181017190602_create_predictions.exs
@@ -0,0 +1,18 @@
+defmodule Predictions.Repo.Migrations.CreatePredictions do
+  use Ecto.Migration
+
+  def change do
+    create table("predictions", primary_key: true) do
+      add(:trip_id, :string)
+      add(:is_deleted, :boolean)
+      add(:delay, :integer)
+      add(:arrival_time, :integer)
+      add(:boarding_status, :string)
+      add(:departure_time, :integer)
+      add(:schedule_relationship, :string)
+      add(:stop_id, :string)
+      add(:stop_sequence, :integer)
+      add(:stops_away, :integer)
+    end
+  end
+end
