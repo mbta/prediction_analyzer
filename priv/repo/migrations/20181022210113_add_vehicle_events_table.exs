d178ba4ca3bd1e7adfd58bb601c7f3b9f22ef191
Gabe Durazo <gabe@mbtace.com>
2018-10-22 16:21:11 -0500

    Migration for new vehicle events table

---
+++ priv/repo/migrations/20181022210113_add_vehicle_events_table.exs
@@ -0,0 +1,17 @@
+defmodule Predictions.Repo.Migrations.AddVehicleEventsTable do
+  use Ecto.Migration
+
+  def change do
+    create table("vehicle_events", primary_key: true) do
+      add(:vehicle_id, :string)
+      add(:vehicle_label, :string)
+      add(:is_deleted, :boolean)
+      add(:route_id, :string)
+      add(:direction_id, :integer)
+      add(:trip_id, :string)
+      add(:stop_id, :string)
+      add(:arrival_time, :integer)
+      add(:departure_time, :integer)
+    end
+  end
+end
