d371aa6e6327ae0129a3c370a713b017275d0665
Gabe Durazo <gabe@mbtace.com>
2018-10-22 15:18:18 -0500

    Run migrations automatically on app start in production

---
+++ lib/predictions/release_tasks.ex
@@ -0,0 +1,17 @@
+defmodule Predictions.ReleaseTasks do
+  @moduledoc "Tasks that need to be run on app startup"
+
+  alias Predictions.Repo
+  alias Ecto.Migrator
+
+  def migrate do
+    migrations_path = Application.app_dir(:prediction_analyzer, "priv/repo/migrations")
+    Migrator.run(Repo, migrations_path, :up, all: true)
+  end
+end
+
+defmodule Predictions.ReleaseTasks.NoOp do
+  @moduledoc "Don't do any migrations on non-prod application starts"
+
+  def migrate, do: nil
+end
