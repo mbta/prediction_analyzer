da9f55c1f2c7e4bdcd6fa8c88cb6df9e6cb3fd12
Alex Sghia-Hughes <alex@mbtace.com>
2018-10-25 13:35:48 -0400

    delete all

--- priv/repo/migrations/20181025152446_add_vehicle_event_id_to_predictions.exs
+++ priv/repo/migrations/20181025152446_add_vehicle_event_id_to_predictions.exs
@@ -3,7 +3,7 @@ defmodule PredictionAnalyzer.Repo.Migrations.AddVehicleEventIdToPredictions do
 
   def change do
     alter table("predictions") do
-      add(:vehicle_event_id, references("vehicle_events"))
+      add(:vehicle_event_id, references("vehicle_events"), on_delete: :delete_all)
     end
   end
 end
