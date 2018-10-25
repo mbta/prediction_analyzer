5aecd30daa50d87e11691909b5d35af3676e814a
Alex Sghia-Hughes <alex@mbtace.com>
2018-10-25 11:22:00 -0400

    wip

--- lib/prediction_analyzer_web/router.ex
+++ lib/prediction_analyzer_web/router.ex
@@ -19,6 +19,7 @@ defmodule PredictionAnalyzerWeb.Router do
 
     get("/", PageController, :index)
     get("/predictions", PredictionsController, :index)
+    get("/vehicle_events", VehicleEventsController, :index)
   end
 
   # Other scopes may use custom stacks.
