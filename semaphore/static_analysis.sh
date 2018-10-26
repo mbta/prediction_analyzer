60fc7cb2009477e6263736025f6931e5695b9509
Alex Sghia-Hughes <alex@mbtace.com>
2018-10-26 13:35:50 -0400

    simple static_analysis

--- semaphore/static_analysis.sh
+++ semaphore/static_analysis.sh
@@ -2,13 +2,4 @@
 set -e
 set -x
 
-# run pronto in background
-MIX_ENV=test pronto run -f github github_status -c origin/master &
-pronto_pid=$!
-
-
-export ERL_CRASH_DUMP=/dev/null
-
 mix format mix.exs "lib/**/*.{ex,exs}" "test/**/*.{ex,exs}" --check-formatted
-
-wait $pronto_pid
