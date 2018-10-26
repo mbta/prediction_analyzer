50c234812ea2a6e2666208fb986f38c33889f5a6
Alex Sghia-Hughes <alex@mbtace.com>
2018-10-26 11:29:24 -0400

    less stuff

--- semaphore/static_analysis.sh
+++ semaphore/static_analysis.sh
@@ -8,15 +8,8 @@ pronto_pid=$!
 
 MIX_ENV=test mix coveralls.json
 bash <(curl -s https://codecov.io/bash) -t $PREDICTION_ANALYZER_CODECOV_TOKEN
-MIX_ENV=test mix test --only integration
-
-# copy any pre-built PLTs to the right directory
-find $SEMAPHORE_CACHE_DIR -name "dialyxir_*_deps-test.plt*" | xargs -I{} cp '{}' _build/test
 
 export ERL_CRASH_DUMP=/dev/null
-
-# copy build PLTs back
-cp _build/test/*_deps-test.plt* $SEMAPHORE_CACHE_DIR
 
 mix format mix.exs "lib/**/*.{ex,exs}" "test/**/*.{ex,exs}" --check-formatted
 
