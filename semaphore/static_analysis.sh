e76410f43e87a3fc535ebb95d4c41b61d971f957
Alex Sghia-Hughes <alex@mbtace.com>
2018-10-26 14:07:15 -0400

    rebase over the formatter

--- semaphore/static_analysis.sh
+++ semaphore/static_analysis.sh
@@ -2,15 +2,7 @@
 set -e
 set -x
 
-# run pronto in background
-MIX_ENV=test pronto run -f github github_status -c origin/master &
-pronto_pid=$!
-
 MIX_ENV=test mix coveralls.json
 bash <(curl -s https://codecov.io/bash) -t $PREDICTION_ANALYZER_CODECOV_TOKEN -r mbta/prediction_analyzer
 
-export ERL_CRASH_DUMP=/dev/null
-
-mix format mix.exs "lib/**/*.{ex,exs}" "test/**/*.{ex,exs}" --check-formatted
-
-wait $pronto_pid
+mix format --check-formatted
