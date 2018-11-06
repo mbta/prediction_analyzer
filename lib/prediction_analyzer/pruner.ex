6ec09e17bc355783420211b6be77ff1ee1e6b433
Gabe Durazo <gabe@durazo.us>
2018-11-06 16:06:24 -0600

    Second half of index performance improvements (#37)

--- lib/prediction_analyzer/pruner.ex
+++ lib/prediction_analyzer/pruner.ex
@@ -30,6 +30,8 @@ defmodule PredictionAnalyzer.Pruner do
 
     {time, _} =
       :timer.tc(fn ->
+        Logger.info("deleting old predictions")
+
         Repo.delete_all(
           from(
             p in Prediction,
@@ -37,10 +39,21 @@ defmodule PredictionAnalyzer.Pruner do
           )
         )
 
+        Logger.info("deleting old vehicle events based on arrival")
+
         Repo.delete_all(
           from(
             ve in VehicleEvent,
-            where: ve.arrival_time < ^unix_cutoff or ve.departure_time < ^unix_cutoff
+            where: ve.arrival_time < ^unix_cutoff
+          )
+        )
+
+        Logger.info("deleting old vehicle events based on departure")
+
+        Repo.delete_all(
+          from(
+            ve in VehicleEvent,
+            where: ve.departure_time < ^unix_cutoff
           )
         )
       end)
