a7bd1afec7a503e6de057acc67816265af9ed66f
Gabe Durazo <gabe@durazo.us>
2018-11-06 14:31:40 -0600

    Performance tweaks (#36)
    * Performance tweaks
    
    * Revert non-migration changes for now

---
+++ priv/repo/migrations/20181106155014_add_vehicle_events_indexes.exs
@@ -0,0 +1,10 @@
+defmodule PredictionAnalyzer.Repo.Migrations.AddNecessaryIndexes do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+
+  def change do
+    create index("vehicle_events", [:arrival_time], concurrently: true)
+    create index("vehicle_events", [:departure_time], concurrently: true)
+    create index("predictions", [:trip_id], where: "vehicle_event_id is null", concurrently: true)
+  end
+end
