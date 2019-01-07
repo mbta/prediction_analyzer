f4b17387a0515de5fb9308e5e5da9ed57c94c36c
Gabe Durazo <gabe@mbtace.com>
2019-01-07 16:04:00 -0600

    mix format and add type spec

--- lib/prediction_analyzer_web/controllers/health_controller.ex
+++ lib/prediction_analyzer_web/controllers/health_controller.ex
@@ -1,6 +1,7 @@
 defmodule PredictionAnalyzerWeb.HealthController do
   use PredictionAnalyzerWeb, :controller
 
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     conn
     |> send_resp(200, "Healthy")
