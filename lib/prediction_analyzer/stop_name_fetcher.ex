ddeb9fa94684891ebe7565d940918070919a4271
Phil Darnowsky <pdarnowsky@mbta.com>
2019-03-07 15:12:14 -0500

    Typespecs for everybody

--- lib/prediction_analyzer/stop_name_fetcher.ex
+++ lib/prediction_analyzer/stop_name_fetcher.ex
@@ -2,21 +2,28 @@ defmodule PredictionAnalyzer.StopNameFetcher do
   require Logger
   use GenServer
 
+  @type state() :: [{String.t(), String.t()}]
+
+  @spec start_link([any]) :: {:ok, pid}
   def start_link(opts \\ []) do
     state = get_stop_names()
     GenServer.start_link(__MODULE__, state, opts)
   end
 
+  @spec init(state) :: {:ok, state}
   def init(state) do
     {:ok, state}
   end
 
+  @spec get_stop_map() :: {:reply, state, state}
   def get_stop_map() do
     GenServer.call(__MODULE__, :get_stop_map)
   end
 
+  @spec handle_call(:get_stop_map, GenServer.from(), state) :: {:reply, state, state}
   def handle_call(:get_stop_map, _from, state), do: {:reply, state, state}
 
+  @spec get_stop_names() :: state
   def get_stop_names do
     url = "https://api-v3.mbta.com/stops"
     api_key = Application.get_env(:prediction_analyzer, :api_v3_key)
@@ -35,6 +42,7 @@ defmodule PredictionAnalyzer.StopNameFetcher do
     end
   end
 
+  @spec parse_response(%HTTPoison.Response{}) :: state
   defp parse_response(http_response) do
     {:ok, body} = Jason.decode(http_response.body)
 
