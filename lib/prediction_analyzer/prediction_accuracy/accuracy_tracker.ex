25abcd6d28a466479c64f6d1a702f82c60511cb3
Alex Sghia-Hughes <alex@mbtace.com>
2019-05-03 13:36:14 -0400

    Ash accuracy drop warn (#93)
    * check accuracy daily
    
    * add some tests
    
    * compiler warning
    
    * wip
    
    * check all routes accuracies
    
    * add start link test becaues codecov
    
    * do the testing thing

---
+++ lib/prediction_analyzer/prediction_accuracy/accuracy_tracker.ex
@@ -0,0 +1,100 @@
+defmodule PredictionAnalyzer.PredictionAccuracy.AccuracyTracker do
+  alias PredictionAnalyzer.PredictionAccuracy.PredictionAccuracy
+  require Logger
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, [], opts)
+  end
+
+  def init([]) do
+    schedule_next_check(self())
+    {:ok, [yesterdays_accuracy: 0, previous_days_accuracy: 0]}
+  end
+
+  def check_accuracy() do
+    ["Red", "Blue", "Orange", "Green-B", "Green-C", "Green-D", "Green-E", "Mattapan"]
+    |> Enum.reduce(%{}, fn route_id, acc ->
+      Map.put(acc, route_id, get_route_accuracy(route_id))
+    end)
+  end
+
+  @spec get_route_accuracy(String.t()) :: [
+          yesterday_accuracy: float(),
+          previous_day_accuracy: float()
+        ]
+  defp get_route_accuracy(route_id) do
+    today = Date.utc_today()
+    yesterday = today |> Timex.shift(days: -1) |> Date.to_iso8601()
+    previous_day = today |> Timex.shift(days: -2) |> Date.to_iso8601()
+
+    {yesterday_query, _} =
+      PredictionAccuracy.filter(%{
+        "route_id" => route_id,
+        "chart_range" => "Hourly",
+        "service_date" => yesterday
+      })
+
+    yesterday_accs =
+      yesterday_query
+      |> PredictionAccuracy.stats_by_environment_and_chart_range(%{
+        "chart_range" => "Hourly"
+      })
+      |> PredictionAnalyzer.Repo.all()
+
+    {previous_day_query, _} =
+      PredictionAccuracy.filter(%{
+        "route_id" => route_id,
+        "chart_range" => "Hourly",
+        "service_date" => previous_day
+      })
+
+    previous_day_accs =
+      previous_day_query
+      |> PredictionAccuracy.stats_by_environment_and_chart_range(%{
+        "chart_range" => "Hourly"
+      })
+      |> PredictionAnalyzer.Repo.all()
+
+    yesterday_accuracy = get_accuracy(yesterday_accs)
+    previous_day_accuracy = get_accuracy(previous_day_accs)
+
+    if yesterday_accuracy < previous_day_accuracy do
+      Logger.warn(
+        "accuracy_drop on #{route_id} from #{previous_day_accuracy} to #{yesterday_accuracy} between #{
+          previous_day
+        } and #{yesterday}"
+      )
+    end
+
+    [yesterday_accuracy: yesterday_accuracy, previous_day_accuracy: previous_day_accuracy]
+  end
+
+  def handle_info(:schedule_next_check, _state) do
+    state = check_accuracy()
+    schedule_next_check(self())
+    {:noreply, state}
+  end
+
+  @spec schedule_next_check(pid()) :: :ok
+  defp schedule_next_check(pid) do
+    Logger.info("scheduling next check_accuracy")
+    Process.send_after(pid, :schedule_next_check, 24 * 60 * 60 * 1_000)
+    :ok
+  end
+
+  defp get_accuracy(query) do
+    {total, accurate} =
+      Enum.reduce(query, {0, 0}, fn [_, prod_total, prod_accurate, _, _], {total, accurate} ->
+        {total + prod_total, accurate + prod_accurate}
+      end)
+
+    accuracy =
+      if total > 0 do
+        accurate / total
+      else
+        0
+      end
+
+    accuracy
+  end
+end
---
+++ test/prediction_analyzer/prediction_accuracy/accuracy_tracker_test.exs
@@ -0,0 +1,110 @@
+defmodule PredictionAnalyzer.PredictionAccuracy.AccuracyTrackerTest do
+  use ExUnit.Case, async: false
+  alias PredictionAnalyzer.PredictionAccuracy.AccuracyTracker
+  alias PredictionAnalyzer.PredictionAccuracy.PredictionAccuracy
+  alias PredictionAnalyzer.Repo
+  import ExUnit.CaptureLog
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(PredictionAnalyzer.Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(PredictionAnalyzer.Repo, {:shared, self()})
+  end
+
+  describe "start_link/1" do
+    test "starts an instance of the accuracy tracker" do
+      {:ok, pid} = AccuracyTracker.start_link()
+      assert Process.alive?(pid)
+    end
+  end
+
+  describe "handle_info/2 :check_accuracy" do
+    test "logs a warning when accuracy drops from previous day to yesterday" do
+      setup_db(70, 80)
+
+      log =
+        capture_log([level: :warn], fn ->
+          AccuracyTracker.check_accuracy()
+        end)
+
+      assert log =~ "accuracy_drop"
+    end
+
+    test "does not log a warning when accuracy stays the same from previous day to yesterday" do
+      setup_db(80, 80)
+
+      log =
+        capture_log([level: :warn], fn ->
+          AccuracyTracker.check_accuracy()
+        end)
+
+      refute log =~ "accuracy_drop"
+    end
+
+    test "does not log a warning when accuracy rises the same from previous day to yesterday" do
+      setup_db(90, 80)
+
+      log =
+        capture_log([level: :warn], fn ->
+          AccuracyTracker.check_accuracy()
+        end)
+
+      refute log =~ "accuracy_drop"
+    end
+  end
+
+  describe "handle_info/2 :schedule_next_check" do
+    test "checks the accuracy of all lines we care about" do
+      Logger.configure(level: :info)
+
+      log =
+        capture_log([level: :info], fn ->
+          {:ok, pid} = AccuracyTracker.start_link()
+          Ecto.Adapters.SQL.Sandbox.mode(PredictionAnalyzer.Repo, {:shared, pid})
+
+          Process.send_after(pid, :schedule_next_check, 10)
+
+          :timer.sleep(50)
+        end)
+
+      Logger.configure(level: :warn)
+
+      assert log =~ "check_accuracy"
+    end
+  end
+
+  def setup_db(yesterday_accurate, previous_day_accurate) do
+    today = Date.utc_today()
+    yesterday = today |> Timex.shift(days: -1) |> Date.to_iso8601()
+    previous_day = today |> Timex.shift(days: -2) |> Date.to_iso8601()
+
+    changeset =
+      PredictionAccuracy.new_insert_changeset(%{
+        service_date: yesterday,
+        hour_of_day: 5,
+        stop_id: "stop1",
+        route_id: "Red",
+        direction_id: 1,
+        arrival_departure: "arrival",
+        bin: "0-3 min",
+        num_predictions: 100,
+        num_accurate_predictions: yesterday_accurate
+      })
+
+    Repo.insert!(changeset)
+
+    changeset =
+      PredictionAccuracy.new_insert_changeset(%{
+        service_date: previous_day,
+        hour_of_day: 5,
+        stop_id: "stop1",
+        route_id: "Red",
+        direction_id: 1,
+        arrival_departure: "arrival",
+        bin: "0-3 min",
+        num_predictions: 100,
+        num_accurate_predictions: previous_day_accurate
+      })
+
+    Repo.insert!(changeset)
+  end
+end
