a54562c0606d7c7315cef37b401e146cd24665d7
Gabe Durazo <gabe@mbtace.com>
2019-06-26 15:23:34 -0500

    Don't provide defaults for column, so it migrates fast.

--- priv/repo/migrations/20190624192925_add_mse_rmse_columns.exs
+++ priv/repo/migrations/20190624192925_add_mse_rmse_columns.exs
@@ -3,13 +3,13 @@ defmodule PredictionAnalyzer.Repo.Migrations.AddMseRmseColumns do
 
   def change do
     alter table("prediction_accuracy") do
-      add(:mean_error, :real, null: false, default: 0.0)
-      add(:root_mean_squared_error, :real, null: false, default: 0.0)
+      add(:mean_error, :real)
+      add(:root_mean_squared_error, :real)
     end
 
     alter table("weekly_accuracies") do
-      add(:mean_error, :real, null: false, default: 0.0)
-      add(:root_mean_squared_error, :real, null: false, default: 0.0)
+      add(:mean_error, :real)
+      add(:root_mean_squared_error, :real)
     end
   end
 end
