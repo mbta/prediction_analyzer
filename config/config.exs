ada4534311f9212b260c51c3c180c0876a4ff37b
Gabe Durazo <gdurazo@mbta.com>
2020-04-27 10:24:12 -0500

    fix: Reduce raw predictions storage to 12 hrs

--- config/config.exs
+++ config/config.exs
@@ -31,7 +31,7 @@ config :prediction_analyzer, :migration_task, Predictions.ReleaseTasks.NoOp
 config :prediction_analyzer, :stop_name_fetcher, PredictionAnalyzer.StopNameFetcher
 config :prediction_analyzer, :timezone, "America/New_York"
 config :prediction_analyzer, :max_dwell_time_sec, 30 * 60
-config :prediction_analyzer, :prune_lookback_sec, 28 * 24 * 60 * 60
+config :prediction_analyzer, :prune_lookback_sec, 12 * 60 * 60
 
 config :prediction_analyzer, PredictionAnalyzer.Repo,
   adapter: Ecto.Adapters.Postgres,
