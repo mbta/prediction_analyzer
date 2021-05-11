386ca098370369ae110c6a4fd8b5e22fc8e870e4
Gabe Durazo <gdurazo@mbta.com>
2021-05-11 11:45:30 -0500

    chore: Remove CodeCov

--- semaphore/static_analysis.sh
+++ semaphore/static_analysis.sh
@@ -3,7 +3,6 @@ set -e
 set -x
 
 MIX_ENV=test mix coveralls.json
-bash <(curl -s https://codecov.io/bash) -t $PREDICTION_ANALYZER_CODECOV_TOKEN -r mbta/prediction_analyzer
 
 mix format --check-formatted
 npm --prefix assets run check
