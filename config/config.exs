95ea64564880887512c82c2f5fa037ff9a98dedb
Gabe Durazo <gdurazo@mbta.com>
2022-04-27 15:25:20 -0500

    wip chore: update to Ecto 3

--- config/config.exs
+++ config/config.exs
@@ -33,8 +33,6 @@ config :prediction_analyzer, :timezone, "America/New_York"
 config :prediction_analyzer, :max_dwell_time_sec, 30 * 60
 config :prediction_analyzer, :prune_lookback_sec, 7 * 24 * 60 * 60
 
-config :prediction_analyzer, PredictionAnalyzer.Repo, adapter: Ecto.Adapters.Postgres
-
 config :prediction_analyzer, start_workers: true
 
 config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
--- lib/prediction_analyzer/repo.ex
+++ lib/prediction_analyzer/repo.ex
@@ -1,5 +1,5 @@
 defmodule PredictionAnalyzer.Repo do
-  use Ecto.Repo, otp_app: :prediction_analyzer
+  use Ecto.Repo, otp_app: :prediction_analyzer, adapter: Ecto.Adapters.Postgres
 
   def init(_, opts) do
     {:ok, Keyword.put_new(opts, :url, System.get_env("DATABASE_URL"))}
