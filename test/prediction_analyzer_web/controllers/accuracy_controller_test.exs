81e9937dea2137c5c19f311da67c7e208c2e7b0f
Nick Stein <nstein@mbta.com>
2023-01-24 12:20:38 -0500

    fix: use basic text comparison instead of regex

--- test/prediction_analyzer_web/controllers/accuracy_controller_test.exs
+++ test/prediction_analyzer_web/controllers/accuracy_controller_test.exs
@@ -245,8 +245,7 @@ defmodule PredictionAnalyzerWeb.AccuracyControllerTest do
 
     response = html_response(conn, 200)
 
-    assert response =~
-             ~r/0-3 min[[:blank:]]+<\/td>\n[[:blank:]]+<td>\n[[:blank:]]+-60 sec to 60 sec/
+    assert response =~ "<td>\n0-3 min\n              </td>\n              <td>\n-60 sec to 60 sec"
   end
 
   def insert_hourly_accuracy(env, hour, total, accurate, service_date \\ nil) do
