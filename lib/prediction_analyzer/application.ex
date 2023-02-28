5c061a279db35caa8c0784fd25fc4c43c3649d72
Nick Stein <nstein@mbta.com>
2023-02-28 12:41:15 -0500

    chore: add injection points for sentry library

--- lib/prediction_analyzer/application.ex
+++ lib/prediction_analyzer/application.ex
@@ -6,6 +6,7 @@ defmodule PredictionAnalyzer.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
+    Logger.add_backend(Sentry.LoggerBackend)
     set_runtime_config()
     # Define workers and child supervisors to be supervised
     supervisors = [
