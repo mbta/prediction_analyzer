4949a937259d017659b75b70a3c4493bb88b0418
Nick Stein <nstein@mbta.com>
2023-03-01 18:06:37 -0500

    chore: support SENTRY_ENV env var

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,13 +3,14 @@ import Config
 is_prod? = config_env() == :prod
 
 if is_prod? do
+  sentry_env = System.fetch_env!("SENTRY_ENV")
   config :sentry,
     dsn: System.fetch_env!("SENTRY_DSN"),
-    environment_name: :prod,
+    environment_name: sentry_env,
     enable_source_code_context: true,
     root_source_code_path: File.cwd!(),
     tags: %{
-      env: "production"
+      env: sentry_env
     },
-    included_environments: [:prod]
+    included_environments: [sentry_env]
 end
