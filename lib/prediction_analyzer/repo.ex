e0f609e71368b0bd12cccc4a9333760d9256d92d
Jon Zimbel <jzimbel@mbta.com>
2025-01-15 12:09:41 -0500

    Log some things for debugging

--- lib/prediction_analyzer/repo.ex
+++ lib/prediction_analyzer/repo.ex
@@ -17,15 +17,26 @@ defmodule PredictionAnalyzer.Repo do
     token = mod.generate_db_auth_token(hostname, username, port, %{})
     :ok = Logger.info("generated_aws_rds_iam_auth_token")
 
+    certfile_path = Path.join(:code.priv_dir(:prediction_analyzer), "aws-cert-bundle.pem")
+
     Keyword.merge(config,
       password: token,
+      ssl: true,
       ssl_opts: [
-        cacertfile: Path.join(:code.priv_dir(:prediction_analyzer), "aws-cert-bundle.pem"),
+        cacertfile: certfile_path,
         verify: :verify_peer,
         server_name_indication: String.to_charlist(hostname),
         verify_fun:
           {&:ssl_verify_hostname.verify_fun/3, [check_hostname: String.to_charlist(hostname)]}
       ]
     )
+    |> tap(fn conf ->
+      IO.inspect(conf, label: "Repo config")
+      IO.inspect(Keyword.get(conf, :ssl), label: "ssl key")
+      IO.inspect(Keyword.get(conf, :ssl_opts), label: "ssl_opts key")
+      IO.inspect(certfile_path, label: "certfile path")
+      IO.inspect(File.exists?(certfile_path), label: "certfile exists at that path?")
+      IO.inspect(File.regular?(certfile_path), label: "certfile is a file and not dir?")
+    end)
   end
 end
