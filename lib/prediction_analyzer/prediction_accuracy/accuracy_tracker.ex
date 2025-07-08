00fa11ec47ba47ab0df652e6be1522a38d45ebd3
leishab <leisha.bhandari@tufts.edu>
2025-07-08 14:18:22 -0400

    Same-Day Hourly Drop Alert for South Station
    First Attempt at creating a prediction accuracy tracker for South station: Mostly focused on at_terminal and reverse_trip prediction update types.

--- lib/prediction_analyzer/prediction_accuracy/accuracy_tracker.ex
+++ lib/prediction_analyzer/prediction_accuracy/accuracy_tracker.ex
@@ -5,7 +5,13 @@ defmodule PredictionAnalyzer.PredictionAccuracy.AccuracyTracker do
   alias PredictionAnalyzer.Filters
   require Logger
 
-  @drop_threshold 0.1
+  # @drop_threshold 0.1
+
+  @target_stop_id "place-south-station"
+  @check_interval_minutes 30
+  @hourly_drop_threshold 0.25
+  @daily_drop_threshold 0.25
+  @min_accuracy_threshold 0.50
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, [], opts)
@@ -16,60 +22,11 @@ defmodule PredictionAnalyzer.PredictionAccuracy.AccuracyTracker do
     {:ok, [yesterdays_accuracy: 0, previous_days_accuracy: 0]}
   end
 
-  def check_accuracy() do
-    ["Red", "Blue", "Orange", "Green-B", "Green-C", "Green-D", "Green-E", "Mattapan"]
-    |> Enum.reduce(%{}, fn route_id, acc ->
-      Map.put(acc, route_id, get_route_accuracy(route_id))
-    end)
-  end
-
-  @spec get_route_accuracy(String.t()) :: [
-          yesterday_accuracy: float(),
-          previous_day_accuracy: float()
-        ]
-  defp get_route_accuracy(route_id) do
-    today = Date.utc_today()
-    yesterday = today |> Timex.shift(days: -1) |> Date.to_iso8601()
-    previous_day = today |> Timex.shift(days: -2) |> Date.to_iso8601()
-
-    {yesterday_query, _} =
-      PredictionAccuracy.filter(%{
-        "route_id" => route_id,
-        "chart_range" => "Hourly",
-        "service_date" => yesterday
-      })
-
-    yesterday_accs =
-      yesterday_query
-      |> Filters.stats_by_environment_and_chart_range("prod", %{
-        "chart_range" => "Hourly"
-      })
-      |> PredictionAnalyzer.Repo.all()
-
-    {previous_day_query, _} =
-      PredictionAccuracy.filter(%{
-        "route_id" => route_id,
-        "chart_range" => "Hourly",
-        "service_date" => previous_day
-      })
-
-    previous_day_accs =
-      previous_day_query
-      |> Filters.stats_by_environment_and_chart_range("prod", %{
-        "chart_range" => "Hourly"
-      })
-      |> PredictionAnalyzer.Repo.all()
-
-    yesterday_accuracy = get_accuracy(yesterday_accs)
-    previous_day_accuracy = get_accuracy(previous_day_accs)
-
-    if yesterday_accuracy < previous_day_accuracy - @drop_threshold do
-      Logger.warn(
-        "accuracy_drop on #{route_id} from #{previous_day_accuracy} to #{yesterday_accuracy} between #{previous_day} and #{yesterday}"
-      )
-    end
-
-    [yesterday_accuracy: yesterday_accuracy, previous_day_accuracy: previous_day_accuracy]
+  @spec schedule_next_check(pid()) :: :ok
+  defp schedule_next_check(pid) do
+    Logger.info("scheduling next check_accuracy: South Station")
+    Process.send_after(pid, :check_accuracy, @check_interval_minutes * 60 * 1000)
+    :ok
   end
 
   def handle_info(:schedule_next_check, _state) do
@@ -78,27 +35,126 @@ defmodule PredictionAnalyzer.PredictionAccuracy.AccuracyTracker do
     {:noreply, state}
   end
 
-  @spec schedule_next_check(pid()) :: :ok
-  defp schedule_next_check(pid) do
-    Logger.info("scheduling next check_accuracy")
-    Process.send_after(pid, :schedule_next_check, 24 * 60 * 60 * 1_000)
-    :ok
+  defp check_accuracy() do
+    now = DateTime.utc_now() |> DateTime.truncate(:minute)
+    current_hour_start = DateTime.add(now, -3600, :second)
+    current_hour_end = now
+    previous_hour_start = DateTime.add(current_hour_start, -3600, :second)
+    previous_hour_end = current_hour_start
+    same_hour_yesterday_start = DateTime.add(current_hour_start, -86400, :second)
+    same_hour_yesterday_end = DateTime.add(current_hour_end, -86400, :second)
+
+    accuracy_now = fetch_accuracy(@target_stop_id, current_hour_start, current_hour_end)
+    previous_hour_accuracy = fetch_accuracy(@target_stop_id, previous_hour_start, previous_hour_end)
+    same_hour_yesterday_accuracy = fetch_accuracy(@target_stop_id, same_hour_yesterday_start, same_hour_yesterday_end)
+
+  defp fetch_accuracy(stop_id, start_time, end_time) do
+    PredictionAccuracy.get_accuracy(stop_id, start_time, end_time, ["at_terminal", "reverse_trip"])
   end
 
-  defp get_accuracy(query) do
-    {total, accurate} =
-      Enum.reduce(query, {0, 0}, fn [_, prod_total, prod_accurate, _mean_err, _rmse],
-                                    {total, accurate} ->
-        {total + prod_total, accurate + prod_accurate}
-      end)
+  defp check_alert(accuracy_now, previous_hour_accuracy, same_hour_yesterday_accuracy) do
+    if (previous_hour_accuracy - accuracy_now) > @hourly_drop_threshold do
+      send_alert("Hourly accuracy drop more than 25%: #{previous_hour_accuracy} - #{accuracy_now}")
+    end
 
-    accuracy =
-      if total > 0 do
-        accurate / total
-      else
-        0
-      end
+    if (same_hour_yesterday_accuracy - accuracy_now) > @daily_drop_threshold do
+      send_alert("Daily accuracy drop more than 25%: #{same_hour_yesterday_accuracy} - #{accuracy_now}")
+    end
 
-    accuracy
+    if accuracy_now < @min_accuracy_threshold do
+      send_alert("General accuracy less than 50%: #{accuracy_now}")
+    end
+  end
+
+  defp send_alert(message) do
+    Logger.warn(message)
+    Slack.send_alert("ALERT!!! #{message}", "#tid-transit-data-alerts-testing")
+  end
+
+  # def check_accuracy() do
+  #  ["Red", "Blue", "Orange", "Green-B", "Green-C", "Green-D", "Green-E", "Mattapan"]
+  #  |> Enum.reduce(%{}, fn route_id, acc ->
+  #    Map.put(acc, route_id, get_route_accuracy(route_id))
+  #  end)
+  # end - being commented out
+
+# @spec get_route_accuracy(String.t()) :: [
+#         yesterday_accuracy: float(),
+#         previous_day_accuracy: float()
+#         ]
+#   defp get_route_accuracy(route_id) do
+#     today = Date.utc_today()
+#     yesterday = today |> Timex.shift(days: -1) |> Date.to_iso8601()
+#     previous_day = today |> Timex.shift(days: -2) |> Date.to_iso8601()
+
+#     {yesterday_query, _} =
+#       PredictionAccuracy.filter(%{
+#         "route_id" => route_id,
+#         "chart_range" => "Hourly",
+#         "service_date" => yesterday
+#       })
+
+#     yesterday_accs =
+#       yesterday_query
+#       |> Filters.stats_by_environment_and_chart_range("prod", %{
+#         "chart_range" => "Hourly"
+#       })
+#       |> PredictionAnalyzer.Repo.all()
+
+#     {previous_day_query, _} =
+#       PredictionAccuracy.filter(%{
+#         "route_id" => route_id,
+#         "chart_range" => "Hourly",
+#         "service_date" => previous_day
+#       })
+
+#     previous_day_accs =
+#       previous_day_query
+#       |> Filters.stats_by_environment_and_chart_range("prod", %{
+#         "chart_range" => "Hourly"
+#       })
+#       |> PredictionAnalyzer.Repo.all()
+
+#     yesterday_accuracy = get_accuracy(yesterday_accs)
+#     previous_day_accuracy = get_accuracy(previous_day_accs)
+
+#     if yesterday_accuracy < previous_day_accuracy - @drop_threshold do
+#       Logger.warn(
+#         "accuracy_drop on #{route_id} from #{previous_day_accuracy} to #{yesterday_accuracy} between #{previous_day} and #{yesterday}"
+#       )
+#     end
+
+#     [yesterday_accuracy: yesterday_accuracy, previous_day_accuracy: previous_day_accuracy]
+#   end
+
+#   # @spec schedule_next_check(pid()) :: :ok
+#   # defp schedule_next_check(pid) do
+#   #   Logger.info("scheduling next check_accuracy")
+#   #   Process.send_after(pid, :schedule_next_check, 24 * 60 * 60 * 1_000)
+#   #  :ok
+#   # end
+
+#   @spec schedule_next_check(pid()) :: :ok
+#   defp schedule_next_check(pid) do
+#     Logger.info("scheduling next check_accuracy: South Station")
+#     Process.send_after(pid, :check_accuracy, @check_interval_minutes * 60 * 1000)
+#     :ok
+#   end
+
+#   defp get_accuracy(query) do
+#     {total, accurate} =
+#       Enum.reduce(query, {0, 0}, fn [_, prod_total, prod_accurate, _mean_err, _rmse],
+#                                     {total, accurate} ->
+#         {total + prod_total, accurate + prod_accurate}
+#       end)
+
+#     accuracy =
+#       if total > 0 do
+#         accurate / total
+#       else
+#         0
+#       end
+
+#     accuracy
   end
 end
