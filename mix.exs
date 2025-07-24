d70e352fc24277eeac4aeb70152812eca8bf6172
Kayla Firestack <kfirestack@mbta.com>
2025-07-24 17:33:04 -0400

    feat(update/elixir/1.18): update sentry 9 -> 10
    Still fixing dependency compile issues
    
    ```
    ==> sentry
    Compiling 23 files (.ex)
    
    == Compilation error in file lib/sentry/config.ex ==
    ** (ArgumentError) cannot inject attribute @source_code_exclude_patterns into function/macro because cannot escape #Reference<0.366074352.3786539012.118020>. The supported values are: lists, tuples, maps, atoms, numbers, bitstrings, PIDs and remote functions in the format &Mod.fun/arity
        (elixir 1.18.4) lib/kernel.ex:3777: Kernel.do_at_escape/2
        (elixir 1.18.4) expanding macro: Kernel.@/1
        lib/sentry/config.ex:181: Sentry.Config.source_code_exclude_patterns/0
    could not compile dependency :sentry, "mix compile" failed. Errors may have been logged above. You can recompile this dependency with "mix deps.compile sentry --force", update it with "mix deps.update sentry" or clean it with "mix deps.clean sentry"
    ```

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule PredictionAnalyzer.Mixfile do
       {:plug, "~> 1.10"},
       {:postgrex, ">= 0.0.0"},
       {:timex, "~> 3.1"},
-      {:sentry, "~> 9.0"},
+      {:sentry, "~> 10.0"},
       {:csv, "~> 3.0"}
     ]
   end
