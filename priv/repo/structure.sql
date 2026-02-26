ceaa4112f630c36292ef17df8c0d5bd3b3276257
Jon Zimbel <jzimbel@mbta.com>
2026-02-26 11:53:38 -0500

    fix: Commit structure.sql after running all new migrations

--- priv/repo/structure.sql
+++ priv/repo/structure.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict cEJT4NTbLvdctwtcQZwLW4fwjMcu7oiMQHH18n9Ba8SF6yUvZjo8gj2Pbo6SM76
+\restrict Hk2rc4iOaFdBlM7PitITUIThf2cw5EIQhJU5MM62fg1tzbEm5i5Vg7v2i6Ky4f7
 
 -- Dumped from database version 15.14 (Homebrew)
 -- Dumped by pg_dump version 15.14 (Homebrew)
@@ -40,6 +40,21 @@ CREATE TYPE public.environment AS ENUM (
 
 
 --
+-- Name: oban_job_state; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.oban_job_state AS ENUM (
+    'available',
+    'scheduled',
+    'executing',
+    'retryable',
+    'completed',
+    'discarded',
+    'cancelled'
+);
+
+
+--
 -- Name: prediction_bin; Type: TYPE; Schema: public; Owner: -
 --
 
@@ -66,6 +81,76 @@ CREATE TYPE public.prediction_kind AS ENUM (
 
 
 SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: oban_jobs; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.oban_jobs (
+    id bigint NOT NULL,
+    state public.oban_job_state DEFAULT 'available'::public.oban_job_state NOT NULL,
+    queue text DEFAULT 'default'::text NOT NULL,
+    worker text NOT NULL,
+    args jsonb DEFAULT '{}'::jsonb NOT NULL,
+    errors jsonb[] DEFAULT ARRAY[]::jsonb[] NOT NULL,
+    attempt integer DEFAULT 0 NOT NULL,
+    max_attempts integer DEFAULT 20 NOT NULL,
+    inserted_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    scheduled_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    attempted_at timestamp without time zone,
+    completed_at timestamp without time zone,
+    attempted_by text[],
+    discarded_at timestamp without time zone,
+    priority integer DEFAULT 0 NOT NULL,
+    tags text[] DEFAULT ARRAY[]::text[],
+    meta jsonb DEFAULT '{}'::jsonb,
+    cancelled_at timestamp without time zone,
+    CONSTRAINT attempt_range CHECK (((attempt >= 0) AND (attempt <= max_attempts))),
+    CONSTRAINT positive_max_attempts CHECK ((max_attempts > 0)),
+    CONSTRAINT queue_length CHECK (((char_length(queue) > 0) AND (char_length(queue) < 128))),
+    CONSTRAINT worker_length CHECK (((char_length(worker) > 0) AND (char_length(worker) < 128)))
+);
+
+
+--
+-- Name: TABLE oban_jobs; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE public.oban_jobs IS '12';
+
+
+--
+-- Name: oban_jobs_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.oban_jobs_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: oban_jobs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.oban_jobs_id_seq OWNED BY public.oban_jobs.id;
+
+
+--
+-- Name: oban_peers; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE UNLOGGED TABLE public.oban_peers (
+    name text NOT NULL,
+    node text NOT NULL,
+    started_at timestamp without time zone NOT NULL,
+    expires_at timestamp without time zone NOT NULL
+);
+
 
 --
 -- Name: prediction_accuracy; Type: TABLE; Schema: public; Owner: -
@@ -110,8 +195,6 @@ CREATE SEQUENCE public.prediction_accuracy_id_seq
 
 ALTER SEQUENCE public.prediction_accuracy_id_seq OWNED BY public.prediction_accuracy.id;
 
-
-SET default_table_access_method = heap;
 
 --
 -- Name: prediction_accuracy_default; Type: TABLE; Schema: public; Owner: -
@@ -735,6 +818,13 @@ ALTER TABLE ONLY public.prediction_accuracy ATTACH PARTITION public.prediction_a
 
 
 --
+-- Name: oban_jobs id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.oban_jobs ALTER COLUMN id SET DEFAULT nextval('public.oban_jobs_id_seq'::regclass);
+
+
+--
 -- Name: prediction_accuracy id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -753,6 +843,30 @@ ALTER TABLE ONLY public.predictions ALTER COLUMN id SET DEFAULT nextval('public.
 --
 
 ALTER TABLE ONLY public.vehicle_events ALTER COLUMN id SET DEFAULT nextval('public.vehicle_events_id_seq'::regclass);
+
+
+--
+-- Name: oban_jobs non_negative_priority; Type: CHECK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE public.oban_jobs
+    ADD CONSTRAINT non_negative_priority CHECK ((priority >= 0)) NOT VALID;
+
+
+--
+-- Name: oban_jobs oban_jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.oban_jobs
+    ADD CONSTRAINT oban_jobs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: oban_peers oban_peers_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.oban_peers
+    ADD CONSTRAINT oban_peers_pkey PRIMARY KEY (name);
 
 
 --
@@ -921,6 +1035,27 @@ ALTER TABLE ONLY public.schema_migrations
 
 ALTER TABLE ONLY public.vehicle_events
     ADD CONSTRAINT vehicle_events_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: oban_jobs_args_index; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX oban_jobs_args_index ON public.oban_jobs USING gin (args);
+
+
+--
+-- Name: oban_jobs_meta_index; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX oban_jobs_meta_index ON public.oban_jobs USING gin (meta);
+
+
+--
+-- Name: oban_jobs_state_queue_priority_scheduled_at_id_index; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX oban_jobs_state_queue_priority_scheduled_at_id_index ON public.oban_jobs USING btree (state, queue, priority, scheduled_at, id);
 
 
 --
@@ -1607,7 +1742,7 @@ ALTER TABLE ONLY public.predictions
 -- PostgreSQL database dump complete
 --
 
-\unrestrict cEJT4NTbLvdctwtcQZwLW4fwjMcu7oiMQHH18n9Ba8SF6yUvZjo8gj2Pbo6SM76
+\unrestrict Hk2rc4iOaFdBlM7PitITUIThf2cw5EIQhJU5MM62fg1tzbEm5i5Vg7v2i6Ky4f7
 
 INSERT INTO public."schema_migrations" (version) VALUES (20181017190602);
 INSERT INTO public."schema_migrations" (version) VALUES (20181022210113);
@@ -1638,3 +1773,4 @@ INSERT INTO public."schema_migrations" (version) VALUES (20230914092018);
 INSERT INTO public."schema_migrations" (version) VALUES (20231005191442);
 INSERT INTO public."schema_migrations" (version) VALUES (20260126145616);
 INSERT INTO public."schema_migrations" (version) VALUES (20260218204959);
+INSERT INTO public."schema_migrations" (version) VALUES (20260225041349);
